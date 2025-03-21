3d9bc66b0ccb749e3f98b310c495865d45dc56d4
Zsombor Hajzer <zsombor.hajzer@student.nhlstenden.com>
2025-03-21 20:05:41 +0100

    configuration changes to make the project more consistent, and resolved dependency issues with mysql package -> was changed to Pomelo

--- WeatherThreading.Server/Program.cs
+++ WeatherThreading.Server/Program.cs
@@ -1,21 +1,34 @@
 using Microsoft.EntityFrameworkCore;
 using WeatherThreading.Models;
+using Pomelo.EntityFrameworkCore.MySql;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure database
 builder.Services.AddDbContext<WeatherContext>(opt =>
-    opt.UseInMemoryDatabase("WeatherData"));
+    opt.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+    ));
+
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
-
-app.UseDefaultFiles();
-app.UseStaticFiles();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -24,12 +37,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors();
+app.UseDefaultFiles();
+app.UseStaticFiles();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.MapFallbackToFile("/index.html");
+
+// Configure the port
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"http://+:{port}");
 
 app.Run();
